8fb70261a335739c6a84224a25382604b63e732f
mauroporras <mauroporrasc@gmail.com>
2020-09-15 08:30:47 -0500

    WIP: umd-support

--- rollup.config.js
+++ rollup.config.js
@@ -42,8 +42,8 @@ export default [
     input: 'src/zea/index.js',
     external,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      { file: pkg.main, format: 'cjs', sourcemap },
+      { file: pkg.module, format: 'es', sourcemap },
     ],
     plugins,
   },
